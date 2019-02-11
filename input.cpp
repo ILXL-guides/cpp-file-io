45d79ce71671428900f97353e1de37eaaf7525ca
Paul Inventado <pinventado@fullerton.edu>
2019-02-11 15:10:25 -0800

    use \n instead of endl

--- input.cpp
+++ input.cpp
@@ -12,13 +12,13 @@ int main() {
 
   // read and display string data from the file
   inputFile >> line;
-  std::cout << "I read the following string: " << line << endl;
+  std::cout << "I read the following string: " << line << "\n";
 
   // read and display two integer values from the file
   inputFile >> num;
-  std::cout << "This is the first int: " << num << endl;
+  std::cout << "This is the first int: " << num << "\n";
   inputFile >> num;
-  std::cout << "This is the second int: " << num << endl;
+  std::cout << "This is the second int: " << num << "\n";
 
   inputFile.close();
 

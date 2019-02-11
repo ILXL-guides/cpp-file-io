33e3caa7b820399da9981e50461157cddba2dc5e
Paul Inventado <pinventado@fullerton.edu>
2019-02-11 15:01:15 -0800

    use std::

--- array_input.cpp
+++ array_input.cpp
@@ -1,10 +1,8 @@
 #include <fstream>
 #include <iostream>
 
-using namespace std;
-
 int main() {
-  ifstream inputfile;
+  std::ifstream inputfile;
   inputfile.open("array_output.txt");
   int size = 0;
   inputfile >> size; // identify the number of elements and use it to define the array size
@@ -16,7 +14,7 @@ int main() {
 
   // Display data stored in the array
   for (int i = 0; i < size; i++)
-    cout << arr_values[i] << "\n";
+    std::cout << arr_values[i] << "\n";
     
   inputfile.close();
   return 0;

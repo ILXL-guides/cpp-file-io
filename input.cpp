9b56141dec05a7f299a3f173c7c4d94e1c4d6039
Paul Inventado <pinventado@fullerton.edu>
2019-02-11 15:02:51 -0800

    use std::

--- input.cpp
+++ input.cpp
@@ -2,24 +2,23 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-using namespace std;
 
 int main() {
   string line;
   int num;
 
-  ifstream inputFile; // variable holding connection to the input file
+  std::ifstream inputFile; // variable holding connection to the input file
   inputFile.open("output.txt");
 
   // read and display string data from the file
   inputFile >> line;
-  cout << "I read the following string: " << line << endl;
+  std::cout << "I read the following string: " << line << endl;
 
   // read and display two integer values from the file
   inputFile >> num;
-  cout << "This is the first int: " << num << endl;
+  std::cout << "This is the first int: " << num << endl;
   inputFile >> num;
-  cout << "This is the second int: " << num << endl;
+  std::cout << "This is the second int: " << num << endl;
 
   inputFile.close();
 

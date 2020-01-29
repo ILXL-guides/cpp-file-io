95d8c148ec9b7673c95e156d13215b3854f893cd
Paul Inventado <pinventado@fullerton.edu>
2020-01-29 10:12:55 -0800

    Provide description

--- output.cpp
+++ output.cpp
@@ -1,16 +1,16 @@
 // This program writes a string and 2 integer
 // values to a file
-#include <fstream>
+#include <fstream> // Include fstream to access file-related functions
 #include <iostream>
 
 int main() {
-  std::ofstream output_file; // variable holding connection to an output file
-  output_file.open("output.txt");
+  std::ofstream output_file; // Create an output filestream that will direct data into a file
+  output_file.open("output.txt"); // Open a file given a filename
 
   // write data to the output file (1 string and 2 integers)
-  output_file << "Hello!\n";
-  output_file << 0 << " " << 1 << "\n";
+  output_file << "Hello!\n"; // Use the insertion operator to send data to the file
+  output_file << 0 << " " << 1 << "\n"; 
 
-  output_file.close();
+  output_file.close(); // Close the file to ensure all data is written properly
   return 0;
 }

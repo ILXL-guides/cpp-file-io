116dc5411dd0023cc2c8f701179c104f389d598e
Paul Inventado <pinventado@fullerton.edu>
2020-01-29 10:15:49 -0800

    Provide description

--- input.cpp
+++ input.cpp
@@ -1,5 +1,5 @@
 // This program reads the data from an input file
-#include <fstream>
+#include <fstream> // Include fstream to access file-related functions
 #include <iostream>
 #include <string>
 
@@ -7,12 +7,13 @@ int main() {
   std::string line;
   int num;
 
-  std::ifstream input_file; // variable holding connection to the input file
-  input_file.open("output.txt");
+  std::ifstream input_file; // Create an input filestream that will load data from a file
+  input_file.open("output.txt"); // Open a file given a filename
 
   // read and display string data from the file
-  input_file >> line;
-  std::cout << "I read the following string: " << line << "\n";
+  input_file >> line; // Use the extraction operator to retrieve data and store it to a variable
+                      // The type of data retrieved is based on the variable's data type.
+  std::cout << "I read the following string: " << line << "\n"; // You can use the data stored in the variable accordingly
 
   // read and display two integer values from the file
   input_file >> num;
@@ -20,7 +21,7 @@ int main() {
   input_file >> num;
   std::cout << "This is the second int: " << num << "\n";
 
-  input_file.close();
+  input_file.close(); // Close the file to avoid corruption
 
   return 0;
 }

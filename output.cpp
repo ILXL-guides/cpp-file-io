2afbfe94c4477c8fbf3e78e7b357248a2b6bcaa3
Paul Inventado <pinventado@fullerton.edu>
2020-01-29 09:50:47 -0800

    Use snake case

--- output.cpp
+++ output.cpp
@@ -4,13 +4,13 @@
 #include <iostream>
 
 int main() {
-  std::ofstream outputFile; // variable holding connection to an output file
-  outputFile.open("output.txt");
+  std::ofstream output_file; // variable holding connection to an output file
+  output_file.open("output.txt");
 
   // write data to the output file (1 string and 2 integers)
-  outputFile << "Hello!\n";
-  outputFile << 0 << " " << 1 << "\n";
+  output_file << "Hello!\n";
+  output_file << 0 << " " << 1 << "\n";
 
-  outputFile.close();
+  output_file.close();
   return 0;
 }
